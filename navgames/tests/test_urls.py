e92b0205a4248d64c33a5141ba8f27cf6b78c67f
Cristina <cristina.luis@gmail.com>
2016-10-06 23:01:03 +0200

    updated templates

---
+++ navgames/tests/test_urls.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from django.conf.urls import url, patterns, include
+
+urlpatterns = patterns('', url(r'^admin/', include(admin.site.urls)),
+                       url(r'', include('cms.urls')), )
