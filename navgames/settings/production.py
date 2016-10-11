f9e364ab83b1db4a3344733a5b21c2d8ee610353
Cristina Luis <cristina.luis@gmail.com>
2016-10-11 22:03:37 +0000

    production settings

--- navgames/settings/production.py
+++ navgames/settings/production.py
@@ -2,3 +2,14 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+ALLOWED_HOSTS = ['.navigationgames.org']
+
+STATIC_ROOT = "/home/boojum/navgames-static/"
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': '/home/boojum/navgames/project.db'
+	}
+}
