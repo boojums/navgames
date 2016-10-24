7e05b4fa9a3d7071e6f47b328a08775a6d033eb3
Cristina <cristina.luis@gmail.com>
2016-10-24 10:02:43 +0200

    move apps into app dir

--- manage.py
+++ manage.py
@@ -3,6 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
+
+    # Add apps/ to path
+    sys.path[0:0] = [
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "apps")
+    ]
+
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navgames.settings")
 
     from django.core.management import execute_from_command_line
