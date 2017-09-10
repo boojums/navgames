678e0d0af26e90534cc2e8df913b694f19d1796d
Cristina <cristina.luis@gmail.com>
2017-09-10 12:31:21 +0200

    settings changes to production

--- navgames/settings/base.py
+++ navgames/settings/base.py
@@ -249,22 +249,14 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
 
 DATABASES = {
-    # 'default': {
-    #     'CONN_MAX_AGE': 0,
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'HOST': 'localhost',
-    #     'NAME': 'project.local.db',
-    #     'PASSWORD': '',
-    #     'PORT': '',
-    #     'USER': ''
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.local.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
     }
 }
 
--- navgames/settings/production.py
+++ navgames/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -10,8 +11,12 @@ SECURE_SSL_REDIRECT = True
 STATIC_ROOT = "/home/boojum/navgames-static/"
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': '/home/boojum/navgames/project.db'
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
 }
